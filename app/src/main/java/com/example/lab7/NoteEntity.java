262fb529ecbf8771409e29389479f8ffe321bf9e
ws-23@gmail.com <WS-23@gmail.com>
2025-09-12 16:19:00 +0700

    LAB78

---
+++ app/src/main/java/com/example/lab7/NoteEntity.java
@@ -0,0 +1,31 @@
+package com.example.lab7;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "notes")
+public class NoteEntity {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String title;
+
+    @NonNull
+    public String type; // "text" หรือ "checklist"
+
+    public String checklistItemsJson; // null ถ้าเป็น textnote
+    public String content; // null ถ้าเป็น checklist
+
+    public Date createdDate;
+
+    public NoteEntity(@NonNull String title, @NonNull String type, String checklistItemsJson, String content, Date createdDate) {
+        this.title = title;
+        this.type = type;
+        this.checklistItemsJson = checklistItemsJson;
+        this.content = content;
+        this.createdDate = createdDate;
+    }
+}
